e94dd1bad9f9d2aa11ef1ebd79d983a00e2ab502
ArtemShuhat <shuhat.artem2006@gmail.com>
2026-03-23 21:10:30 +0100

    del separator + new mail

--- back-end/src/config/mailer.config.ts
+++ back-end/src/config/mailer.config.ts
@@ -1,21 +1,36 @@
 import { MailerOptions } from '@nestjs-modules/mailer'
 import { ConfigService } from '@nestjs/config'
 
-import { isDev } from '@/libs/common/utils/is-dev.util'
+const SMTP_SSL_PORTS = new Set([465, 2465])
+
+const parseBoolean = (value?: string) => value?.toLowerCase() === 'true'
 
 export const getMailerConfig = async (
 	configService: ConfigService
-): Promise<MailerOptions> => ({
-	transport: {
-		host: configService.getOrThrow<string>('MAIL_HOST'),
-		port: configService.getOrThrow<number>('MAIL_PORT'),
-		secure: !isDev(configService),
-		auth: {
-			user: configService.getOrThrow<string>('MAIL_LOGIN'),
-			pass: configService.getOrThrow<string>('MAIL_PASSWORD')
+): Promise<MailerOptions> => {
+	const port = Number(configService.getOrThrow<string>('MAIL_PORT'))
+	const secureFromEnv = configService.get<string>('MAIL_SECURE')
+	const secure =
+		secureFromEnv !== undefined
+			? parseBoolean(secureFromEnv)
+			: SMTP_SSL_PORTS.has(port)
+
+	const from =
+		configService.get<string>('MAIL_FROM') ??
+		`"Online shop team" <${configService.getOrThrow<string>('MAIL_LOGIN')}>`
+
+	return {
+		transport: {
+			host: configService.getOrThrow<string>('MAIL_HOST'),
+			port,
+			secure,
+			auth: {
+				user: configService.getOrThrow<string>('MAIL_LOGIN'),
+				pass: configService.getOrThrow<string>('MAIL_PASSWORD')
+			}
+		},
+		defaults: {
+			from
 		}
-	},
-	defaults: {
-		from: `"Online shop team" ${configService.getOrThrow<string>('MAIL_LOGIN')}`
 	}
-})
+}
